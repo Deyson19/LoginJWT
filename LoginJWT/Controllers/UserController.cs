9ca3bf3dcd10902bffa0f6269b46261fd208785b
Deyson Venté <deiandresvent@hotmail.com>
2024-12-18 11:32:58 -0500

    Hash user password before saving
    Added password hashing using `HashPassword.GenerateHash` before saving the user to the database in the `Post` method of `UserController`.

--- LoginJWT/Controllers/UserController.cs
+++ LoginJWT/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 
+using LoginJWT.Helpers;
 using LoginJWT.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,46 +7,47 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LoginJWT.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UserController : ControllerBase
-    {
-        private readonly LoginDbContext _dbContext;
+     [ApiController]
+     [Route("api/[controller]")]
+     public class UserController : ControllerBase
+     {
+          private readonly LoginDbContext _dbContext;
 
-        public UserController(LoginDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+          public UserController(LoginDbContext dbContext)
+          {
+               _dbContext = dbContext;
+          }
 
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
+          [HttpGet]
+          [Authorize(Roles = "Admin")]
 
-        public async Task<IActionResult> GetAll()
-        {
-            var result = await _dbContext.Users.ToListAsync();
-            return Ok(result);
-        }
-        [HttpGet("GetAll")]
-        //[Authorize(Roles = "Client")]
+          public async Task<IActionResult> GetAll()
+          {
+               var result = await _dbContext.Users.ToListAsync();
+               return Ok(result);
+          }
+          [HttpGet("GetAll")]
+          //[Authorize(Roles = "Client")]
 
-        public async Task<IActionResult> GetAllAny()
-        {
-            var result = await _dbContext.Users.ToListAsync();
-            return Ok(new { isSuccess = result.Any(), total = result.Count(), data = result });
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Post([FromBody] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                var response = await _dbContext.Users.AddAsync(user);
-                await _dbContext.SaveChangesAsync();
+          public async Task<IActionResult> GetAllAny()
+          {
+               var result = await _dbContext.Users.ToListAsync();
+               return Ok(new { isSuccess = result.Any(), total = result.Count(), data = result });
+          }
+          [HttpPost]
+          [AllowAnonymous]
+          public async Task<IActionResult> Post([FromBody] User user)
+          {
+               if (ModelState.IsValid)
+               {
+                    user.Password = HashPassword.GenerateHash(user.Password);
+                    await _dbContext.Users.AddAsync(user);
+                    await _dbContext.SaveChangesAsync();
 
-                return Ok(new { message = "user created" });
-            }
-            return BadRequest();
-        }
+                    return Ok(new { message = "user created" });
+               }
+               return BadRequest();
+          }
 
-    }
+     }
 }
